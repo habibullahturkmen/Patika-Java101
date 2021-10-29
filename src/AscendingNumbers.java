87355e732e791ab27e101cbeeb914c795b7ab5ff
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-10-29 09:32:22 +0430

    Pratik - Sayıları Büyükten Küçüğe Sıralama

---
+++ src/AscendingNumbers.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AscendingNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter three digits: ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        if(a < b && a < c) {
+            if (b < c) {
+                System.out.println(a + " < " + b + " < " + c);
+            } else {
+                System.out.println(a + " < " + c + " < " + b);
+            }
+        } else if (b < a && b < c) {
+            if (a < c) {
+                System.out.println(b + " < " + a + " < " + c);
+            } else {
+                System.out.println(b + " < " + c + " < " + a);
+            }
+        } else if (a == b && b == c) {
+            System.out.println(a + " = " + b + " = " + c);
+        } else {
+            if (a < b) {
+                System.out.println(c + " < " + a + " < " + b);
+            } else {
+                System.out.println(c + " < " + b + " < " + a);
+            }
+        }
+    }
+}

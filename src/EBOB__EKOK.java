faea660436d15d1d30595cc0389d25ef011c8b0a
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-11-05 11:30:08 +0430

    Pratik - EBOB-EKOK Bulan Program

--- src/EBOB__EKOK.java
+++ src/EBOB__EKOK.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class EBOB__EKOK {
+
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter two numbers: ");
+
         int n1 = input.nextInt();
         int n2 = input.nextInt();
+
         int minimum = Math.min(n1, n2);
         int maximum = Math.max(n1, n2);
 
         int ebobValue = 1;
         int i = 1;
+
         while (i <= minimum) {
             if(minimum % i == 0 && maximum % i == 0)
             {
@@ -19,9 +24,11 @@ public class EBOB__EKOK {
             }
             i++;
         }
+
         System.out.println("EBOB: " + ebobValue);
 
         int j = 1;
+
         while (j <= maximum * minimum) {
             if(j % maximum == 0 && j % minimum == 0) {
                 System.out.println("EKOK: " + j);
